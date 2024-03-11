9940e75ea30fe45c0ded733f3d873f7b71da994a
kGablo <krzysztof.gablo@tpay.com>
2024-03-11 13:47:51 +0100

    cardPayment fix

--- Model/Resolver/BlikPayment.php
+++ Model/Resolver/BlikPayment.php
@@ -31,22 +31,22 @@ class BlikPayment implements ResolverInterface
 
             if (true === $this->transactionApiFacade->isOpenApiUse()) {
                 if (isset($transaction['payments']['errors']) && count($transaction['payments']['errors']) > 0) {
-                    return 'error';
+                    return ['redirectUrl' => 'error', 'result' => 'error'];
                 }
 
-                return 'success';
+                return ['redirectUrl' => 'success', 'result' => 'success'];
             }
             $this->checkoutSession->unsQuoteId();
 
             if (!(isset($transaction['result']) && 1 === $transaction['result'])) {
                 $this->tpayService->addCommentToHistory($orderId, 'User has typed wrong blik code and has been redirected to transaction panel in order to finish payment');
 
-                return 'error';
+                return ['redirectUrl' => 'error', 'result' => 'error'];
             }
 
-            return 'success';
+            return ['redirectUrl' => 'success', 'result' => 'success'];
         }
 
-        return 'error';
+        return ['redirectUrl' => 'error', 'result' => 'error'];
     }
 }
