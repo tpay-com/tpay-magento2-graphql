0a8fb03fd8293d60012f188f7b9c24c19680dbd7
Adam Wysocki <adam.wysocki@tpay.com>
2024-12-03 10:22:01 +0100

    Blik alias

--- Model/Resolver/Mutation/BlikPayment.php
+++ Model/Resolver/Mutation/BlikPayment.php
@@ -4,9 +4,11 @@ namespace Tpay\Magento2GraphQl\Model\Resolver\Mutation;
 
 use Magento\Checkout\Model\Session;
 use Magento\Framework\GraphQl\Config\Element\Field;
+use Magento\Framework\GraphQl\Exception\GraphQlAuthorizationException;
 use Magento\Framework\GraphQl\Query\ResolverInterface;
 use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
 use Tpay\Magento2\Model\ApiFacade\Transaction\TransactionApiFacade;
+use Tpay\Magento2\Service\TpayAliasService;
 use Tpay\Magento2\Service\TpayService;
 
 class BlikPayment implements ResolverInterface
@@ -15,11 +17,21 @@ class BlikPayment implements ResolverInterface
     private Session $checkoutSession;
     private TpayService $tpayService;
 
-    public function __construct(TransactionApiFacade $transactionApiFacade, Session $checkoutSession, TpayService $tpayService)
-    {
+    private \Magento\Customer\Model\Session $customerSession;
+    private TpayAliasService $aliasService;
+
+    public function __construct(
+        TransactionApiFacade $transactionApiFacade,
+        Session $checkoutSession,
+        TpayService $tpayService,
+        \Magento\Customer\Model\Session $customerSession,
+        TpayAliasService $aliasService
+    ) {
         $this->transactionApiFacade = $transactionApiFacade;
         $this->checkoutSession = $checkoutSession;
         $this->tpayService = $tpayService;
+        $this->customerSession = $customerSession;
+        $this->aliasService = $aliasService;
     }
 
     public function resolve(Field $field, $context, ResolveInfo $info, ?array $value = null, ?array $args = null)
@@ -28,7 +40,15 @@ class BlikPayment implements ResolverInterface
         $orderId = $args['incrementId'];
 
         if ($orderId) {
-            $transaction = $this->transactionApiFacade->blik($args['transactionId'], $args['blikCode'], $args['blikAlias']);
+            if ($args['blikAlias']) {
+                if ($this->customerSession->getCustomerId()) {
+                    $alias = $this->aliasService->getCustomerAlias($this->customerSession->getCustomerId());
+                } else {
+                    throw new GraphQlAuthorizationException(__('The current customer isn\'t authorized. Try again with authorization token'));
+                }
+            }
+
+            $transaction = $this->transactionApiFacade->blik($args['transactionId'], $args['blikCode'], $alias ?? null);
 
             if (true === $this->transactionApiFacade->isOpenApiUse()) {
                 if (isset($transaction['payments']['errors']) && count($transaction['payments']['errors']) > 0) {
@@ -40,7 +60,10 @@ class BlikPayment implements ResolverInterface
             $this->checkoutSession->unsQuoteId();
 
             if (!(isset($transaction['result']) && 1 === $transaction['result'])) {
-                $this->tpayService->addCommentToHistory($orderId, 'User has typed wrong blik code and has been redirected to transaction panel in order to finish payment');
+                $this->tpayService->addCommentToHistory(
+                    $orderId,
+                    'User has typed wrong blik code and has been redirected to transaction panel in order to finish payment'
+                );
 
                 return ['redirectUrl' => 'error'];
             }
